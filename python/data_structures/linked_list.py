84f5c1fde0002b905a44cd77bddf55b2fc53820e
CODY DE LA TORRE <cdelatorre414@gmail.com>
2022-12-26 21:54:32 -0800

    fini chall 5

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,37 @@
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        stringval = self.head
+        string_values = ""
+        while stringval is not None:
+            string_values += f"{ { stringval.value} } -> "
+            stringval = stringval.next
+        string_values += "NULL"
+        return string_values
+
+    def insert(self, value):
+        node = Node(value, self.head)
+        self.head = node
+
+    def includes(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
 
 
 class TargetError:
