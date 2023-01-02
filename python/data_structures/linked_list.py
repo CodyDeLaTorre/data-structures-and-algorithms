9545f507f2f02f3461e58fbbae35b10b93ee2e1c
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-01-02 08:43:12 -0800

    fini code chall 7

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -44,31 +44,37 @@ class LinkedList:
     def insert_before(self, before, value):
         current = self.head
         previous = None
-        if current is None:
-            raise TargetError
-        while current.value is not before:
-            previous = current
-            current = current.next
+        try:
             if current is None:
                 raise TargetError
-        new_node = Node(value)
-        new_node.next = current
-        if previous is not None:
-            previous.next = new_node
-        if previous is None:
-            self.head = new_node
+            while current.value is not before:
+                previous = current
+                current = current.next
+                if current is None:
+                    raise TargetError
+            new_node = Node(value)
+            new_node.next = current
+            if previous is not None:
+                previous.next = new_node
+            if previous is None:
+                self.head = new_node
+        except Exception as e:
+            raise TargetError(e)
 
     def insert_after(self, after, value):
         current = self.head
-        if current is None:
-            raise TargetError
-        while current.value is not after:
-            current = current.next
+        try:
             if current is None:
                 raise TargetError
-        new_node = Node(value)
-        new_node.next = current.next
-        current.next = new_node
+            while current.value is not after:
+                current = current.next
+                if current is None:
+                    raise TargetError
+            new_node = Node(value)
+            new_node.next = current.next
+            current.next = new_node
+        except Exception as e:
+            raise TargetError(e)
 
     def includes(self, value):
         current = self.head
@@ -83,6 +89,19 @@ class LinkedList:
         while current.next is not None:
             current = current.next
         current.next = Node(value)
+
+    def kth_from_end(self, k):
+        if k < 0:
+            raise TargetError()
+        current = self.head
+        ll_list = []
+        try:
+            while current is not None:
+                ll_list.append(current.value)
+                current = current.next
+            return ll_list[-k - 1]
+        except Exception as e:
+            raise TargetError(e)
 
 
 class TargetError(Exception):
