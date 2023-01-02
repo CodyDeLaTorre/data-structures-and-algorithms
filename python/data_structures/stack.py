e77f04e870f1c2eb18dea76c7289f972db0ab25e
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-01-02 15:27:37 -0800

    fini chal 10

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,40 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
+
+
 class Stack:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is None:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        value = self.top.value
+        self.top = self.top.next
+        return value
+
+    def peek(self):
+        try:
+            return self.top.value
+        except Exception as e:
+            raise InvalidOperationError("Method not allowed on empty collection")
+
+    def is_empty(self):
+        if self.top is None:
+            return True
+        else:
+            return False
---
+++ python/docs/stack_and_queue/README.md
@@ -0,0 +1,23 @@
+# Stacks and Queues
+Using a Linked List as the underlying data storage mechanism, implement both a Stack and a Queue
+
+## Challenge
+Can successfully push onto a stack
+Can successfully push multiple values onto a stack
+Can successfully pop off the stack
+Can successfully empty a stack after multiple pops
+Can successfully peek the next item on the stack
+Can successfully instantiate an empty stack
+Calling pop or peek on empty stack raises exception
+Can successfully enqueue into a queue
+Can successfully enqueue multiple values into a queue
+Can successfully dequeue out of a queue the expected value
+Can successfully peek into a queue, seeing the expected value
+Can successfully empty a queue after multiple dequeues
+Can successfully instantiate an empty queue
+Calling dequeue or peek on empty queue raises exception
+
+## Approach & Efficiency
+same approach I used for linked lists since they are very similar
+
+## Whiteboard
