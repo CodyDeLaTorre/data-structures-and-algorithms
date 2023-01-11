ca72998664acba3275ceff0f7959c9de206d4572
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-01-10 18:18:51 -0800

    why is it not working

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,5 +1,20 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
 
 
-def breadth_first():
-    pass
+def breadth_first(tree):
+    if tree.root is None:
+        return []
+
+    queue = Queue()
+    new_list = []
+    queue.enqueue(tree.root)
+
+    while not queue.is_empty():
+        current = queue.dequeue()
+        new_list.append(current.value)
+        if current.left:
+            queue.enqueue(current.left)
+        if current.right:
+            queue.enqueue(current.right)
+    return new_list
