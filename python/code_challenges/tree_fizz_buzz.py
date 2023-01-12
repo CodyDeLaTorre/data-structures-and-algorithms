2d706177a1ded023847c7b09f27f960621f855f0
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-01-11 21:42:43 -0800

    idk why its failing

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,21 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree
 
 
-def fizz_buzz_tree():
-    pass
+def fizz_buzz_tree(tree):
+    new_tree = tree.clone()
+
+    def fizz_buzz_list(node):
+        if node.value % 3  == 0 and node.value % 5 == 0:
+            node.value = "FizzBuzz"
+        elif node.value % 3  == 0:
+            node.value = "Fizz"
+        elif  node.value % 5  == 0:
+            node .value = "Buzz"
+        else:
+            node.value = str(node.value)
+
+        for x in node.children:
+            fizz_buzz_list(x)
+
+        fizz_buzz_list(new_tree.root)
+        return new_tree
