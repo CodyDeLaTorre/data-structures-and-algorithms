58aa83626e317142b98a301780c3f83ee7727014
CODY DE LA TORRE <cdelatorre414@gmail.com>
2023-02-02 22:40:25 -0800

    fini chal 31

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,25 @@
 from data_structures.hashtable import Hashtable
+import re
 
+def first_repeated_word(str):
+    #clean data
+    try:
+        str = str.lower()
+        str = re.sub(r"[\.\-\?]", "", str)
+        str = re.sub(r"\W", ' ', str)
+        str = str.split()
+        # print(str)
 
-def first_repeated_word():
-    pass
+        words = []
+        repeated_words = []
+        #check if word is in set
+        for word in str:
+            if word in words:
+                repeated_words.append(word)
+                # print(repeated_words)
+            else:
+                words.append(word)
+                # print(words)
+        return repeated_words[0]
+    except:
+        return None
---
+++ python/docs/hashtable_repeated_word/README.md
@@ -0,0 +1,19 @@
+# Hashtable Repeated Words
+
+## Challenge
+
+create a hashtasble method to find a repeated word in a string
+
+## Whiteboard
+
+![whit](whiteboard.png)
+
+### API
+
+This method uses regex to clean the str of unnecessary characters.
+sets new lists to compare to eachother.
+uses a loop to find the repeated word
+
+### resources
+
+Google
